cfef6ef7203f06dd81781d14a60b6b2e43a170e6
Sam Nazari <sam.nazari@gmail.com>
2017-01-12 23:09:21 -0500

    Adding Teixeira's UIO FDI scheme.

--- FDIR/Cybersecurity Models/TeixeiraMS/intrusionDetectionMS.m
+++ FDIR/Cybersecurity Models/TeixeiraMS/intrusionDetectionMS.m
@@ -1,6 +1,8 @@
 %% Sam Nazari
 %  Teixeira MS Thesis Intrusion Detection Model
 %  06-Nov-2016
+%  04-Jan-2017
+%  12-Jan-2017
 
 clear,
 clc
@@ -8,22 +10,22 @@ clc
 %% Simulation parameters
 TSIM = 20;
 
-u1Val = 34.632;
-u2Val = 1641.6;
-u3Val = 29980;
-
-X0 = [0.3412;525.7;525.7;496.2];
-
-z110 = 518.6174;
-z410 = -51365.5370;
-z320 = 472.2;
-z420 = -18391.8
-
-fa1 = 0;
-fa2 = 0;
-fa3 = 1;
-
-f = [fa1;fa2;fa3]
+% u1Val = 34.632;
+% u2Val = 1641.6;
+% u3Val = 29980;
+% 
+% X0 = [0.3412;525.7;525.7;496.2];
+% 
+% z110 = 518.6174;
+% z410 = -51365.5370;
+% z320 = 472.2;
+% z420 = -18391.8;
+% 
+% fa1 = 0;
+% fa2 = 0;
+% fa3 = 1;
+% 
+% f = [fa1;fa2;fa3]
 %% Dynamic system 
 
 L = [
@@ -32,82 +34,93 @@ L = [
     -1  -1  3   0   0   -1  0;
     -1  -1  0   3   0   0  -1;
     0   -1  0   0   2   0  -1;
-    0   0   0   -1  -1  -1  3
+    0   0   -1  0   0   2  -1;
+    0   0   0  -1  -1  -1   3
     ]
 
 A = -L
 
-Bf = [
-    1 0 0;
-    0 1 0; 
-    0 0 1
+B = eye(7)
+Bf=B;
+Bf(2,2)=0
+
+
+bf = [0 1 0 0 0 0 0]'
+
+C = [
+    0 0 0 0 0 0 0;
+    0 1 0 0 0 0 0; 
+    0 0 1 0 0 0 0;
+    0 0 0 1 0 0 0;
+    0 0 0 0 0 0 0;
+    0 0 0 0 0 0 0;
+    0 0 0 0 0 0 0
     ]
 
-%C = [1 0 0 0;0 1 0 0; 0 0 1 0]
+E = [1.0;20.758;0.0;0.0]
 
-%E = [1.0;20.758;0.0;0.0]
+x0 = [0 1 1 0 0 0 0]
 
+%% Construct Fault Vectors
+f1 = [1 0 0 0 0 0 0]'  % Vertex one is the intruder
+f2 = [0 1 0 0 0 0 0]'  % Vertex two is the intruder
+f3 = [0 0 1 0 0 0 0]'  % Vertex three is the intruder
+f4 = [0 0 0 1 0 0 0]'  % Vertex four is the intruder
+f5 = [0 0 0 0 1 0 0]'  % Vertex five is the intruder
+f6 = [0 0 0 0 0 1 0]'  % Vertex six is the intruder
+f7 = [0 0 0 0 0 0 1]'  % Vertex seven is the intruder
+
+%E = [f1 f2 f3 f4 f5 f6 f7]
+%E = [f2 f3 f4 f5 f6 f7]
+E = [f1 f3 f5]
+% Choose the agent to be attacked
+flt1  = 0
+flt2  = 0
+flt3  = 0
+flt4  = 1
+flt5  = 0
+flt6  = 0
+flt7  = 0
+
+% Choose a magnitude for the attack
+f1Val = 10
+f2Val = 10
+f3Val = 10
+f4Val = 10
+f5Val = 10
+f6Val = 10
+f7Val = 10
+
+% Chose the attack time
+tf1   = 2
+tf2   = 2
+tf3   = 2
+tf4   = 2
+tf5   = 2
+tf6   = 5
+tf7   = 7
+
+
+%% Rank conditions
+rank(C*bf)
+rank(bf)
 %% UIO 1
-% H1 = [
-%       21.758        -1.0        0.0;
-%       0.0           1.0         0.0;
-%       0.0           0.0         1;
-%       -2075.8       100.0       0.0
-%       ]
-% 
-% T1 = [
-%       -20.758   1   0   0;
-%       0.0       0   0   0;
-%       0         0   0   0;
-%       2075.8    -100 0  1
-%       ]
-% 
-% F1 = [
-%      -10        0.0         0.0         0.0702;
-%      0.0        -20         0.0         0.0;
-%      0.0        0.0         -30         0.0;
-%      0.0        0.0         0.0         -8.4325
-%      ]
-%  
-%  K1 = [
-%         -278.5724       13.3496     0.0;
-%         0.0             0.0         0.0;
-%         0.0             0.0         0.0;
-%         10031.304       -475.5956   0.7781
-%         ]
-%     
-% ICH1 =eye(3)-C*H1
+
+CE = C*bf
+CEin = inv(CE'*CE)
+H=bf*CEin*CE'
+T = eye(7)-H*C
+A1 = T*A
+
+rank(obsv(A1,C))
+k1 = place(A1,C,[-1,-2,-3,-4,-5,-6,-7])
+%F = A1-k1*C
+F = A+H*C*L-k1*C
+k = k1 + F*H
 
 %% UIO 2
-% H2 = [
-%         1   0   0;
-%         0   1   0;
-%         0   0   0;
-%         0   0   40
-%         ]
-%     
-%  T2 = [
-%         0   0   0   0;
-%         0   0   0   0;
-%         0   0   1   0;
-%         0   0   -40 0
-%         ]
-%     
-% F2 =  [
-%         -20     0       0       0;
-%         0       -30     0       0;
-%         0       0       -10     0.2588;
-%         0       0       0       -11.7645
-%         ]
-%     
-% K2 =    [
-%             0       0       0;
-%             0       0       0;
-%             0       0       -15.9068;
-%             0       0.6344  980.5501
-%          ]
-% 
-%  ICH2 = eye(3)-C*H2
+
+%% UIO 3
  
 %% Sim & Plot
 
